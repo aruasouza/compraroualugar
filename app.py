55ca66204dd5449463b153ce71fcc168c4166b5b
aruasouza <arua.viso@gmail.com>
2023-06-20 17:00:40 -0300

    cc

--- app.py
+++ app.py
@@ -33,6 +33,7 @@ def calculate():
     ipca = st.session_state['ipca'] / 100
     selic = st.session_state['taxabase'] / 100
     selic_mes = ((1 + selic) ** (1 / 12)) - 1
+    aquisicao = (st.session_state['taxaaquisicao'] / 100) * valorimovel
     if st.session_state['metodo'] == 'SAC':
         amort = np.array([0] + [financiamento / meses for i in range(meses)])
         saldo_devedor = np.array([financiamento] * (meses + 1)) - amort.cumsum()
@@ -56,7 +57,7 @@ def calculate():
     df['Resultado Imóvel'] = df['Valorização'] - df['Depreciação']
     df['Valor Imóvel'] = df['Resultado Imóvel'].cumsum() + valorimovel
     df['Aluguel'] = [0] + list(aluguel * ((1 + ipca) ** df['Ano']))[1:]
-    df['Investimento'] = [entrada * ((1 + selic_mes) ** i) for i in range(meses + 1)]
+    df['Investimento'] = [(entrada + aquisicao) * ((1 + selic_mes) ** i) for i in range(meses + 1)]
     df['Juros Investimento'] = df['Investimento'].diff().fillna(0)
     df['Financiamento'] = [financiamento] + ([0] * meses)
     anual_df = df.groupby('Ano').sum()
@@ -129,16 +130,16 @@ with st.sidebar:
         st.number_input('Valor do Imóvel',min_value = 0.0, value = 480000.0, step=1000.0, format='%.2f', key='valorimovel')
         st.number_input('Valorização do Imóvel (% a.a.)',min_value = 0.0, value = 9.1, step=.1, format='%.2f', key='valorizacao')
         st.number_input('Anos Financiamento',min_value = 0, value = 5, step=1, key='anosfinanciamento')
+        st.number_input('Taxa de Aquisição (%)',min_value = 0.0,value = 2.0,step = .1,format='%.2f',key = 'taxaaquisicao')
     with col2:
         st.number_input('Entrada',min_value = 0.0,max_value = st.session_state['valorimovel'], value = 96000.0, step=1000.0, format='%.2f', key='entrada')
         st.number_input('Depreciação (% a.a.)',min_value = 0.0, value = 4.0, step=.1, format='%.2f', key='depreciacao')
         st.number_input('Taxa Financiamento (% a.a.)',min_value = 0.0, value = 9.99, step=1.0, key='taxafinanciamento')
+        st.radio('Método',options = ['SAC','PRICE'],key = 'metodo')
     st.header('Alugar')
     col1,col2 = st.columns(2)
     with col1:
         st.number_input('Valor do Aluguel',min_value = 0.0, value = 1933.28, step=100.0, format='%.2f', key='aluguel')
-    with col2:
-        st.radio('Método',options = ['SAC','PRICE'],key = 'metodo')
     st.header('Variáveis Macroeconômicas')
     col1,col2 = st.columns(2)
     with col1:
